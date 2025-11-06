473b3d0ea43e42223f95f866ef8372a111931bae
Leopold <me@thientd.dev>
2025-11-06 19:07:06 +0700

    Reverse package.json

--- shared/config.lua
+++ shared/config.lua
@@ -1,16 +1,3 @@
-/**
--- HELIX Client 
-TriggerCallback('callbackName', function(result)
-    print(result)
-end, arg1, arg2)
-
--- HELIX Server
-RegisterCallback('callbackName', function(source, arg1, arg2)
-    return returnValue
-end)
-
-**/
-
 SHARED.CONFIG = {
     LANGUAGE = 'en',
     BASE_EXP = 100, -- Base experience for level 1
