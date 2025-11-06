6d4de9bf32a8ad9defb4829e51c7d97bffde071d
Leopold <me@thientd.dev>
2025-11-06 19:46:52 +0700

    Update vite project

--- client/main.lua
+++ client/main.lua
@@ -1,4 +1,4 @@
-TPNRPUI = WebUI('TPNRP-UI', 'tpnrp-core/client/tpnrp-ui/dist/index.html', 0)
+TPNRPUI = WebUI('tpnrp-core', 'tpnrp-core/client/tpnrp-ui/dist/index.html', 0)
 
 ---/********************************/
 ---/*            Main              */
@@ -9,3 +9,10 @@ TPNRPClient = TPNRPClient.new()
 exports('tpnrp-core', 'getCore', function()
     return TPNRPClient
 end)
+
+function onShutdown()
+    if TPNRPUI then
+        TPNRPUI:Destroy()
+        TPNRPUI = nil
+    end
+end
