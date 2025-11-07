d433f0ca204fc9b535b5321e7d3706848bf6fb46
Thien Tran <Thien.Tran@navigosgroup.com>
2025-11-07 20:38:38 +0700

    Update UI

--- client/tpnrp-ui/src/App.tsx
+++ client/tpnrp-ui/src/App.tsx
@@ -1,5 +1,6 @@
 
 import { DevMode } from "./components/debug/DevMode"
+import { HUD } from "./components/game/HUD"
 import { ThemeProvider } from "./components/theme-provider"
 
 declare global {
@@ -14,6 +15,7 @@ function App() {
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <DevMode />
+        <HUD />
       </ThemeProvider>
     </>
   )
---
+++ client/tpnrp-ui/src/components/game/HUD/index.tsx
@@ -0,0 +1,7 @@
+export const HUD = () => {
+  return (
+    <div className="fixed bottom-5 right-5 z-10 rounded-full w-64 h-12 bg-primary! text-primary-foreground!">
+      <h1>HUD</h1>
+    </div>
+  )
+}
