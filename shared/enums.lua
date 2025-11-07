f668141c12294af206f7e241bff87cbf5231357c
Leopold <me@thientd.dev>
2025-11-07 20:39:55 +0700

    Update structure

---
+++ shared/enums.lua
@@ -0,0 +1,23 @@
+---@enum EEquipmentClothType
+EEquipmentClothType = {
+    Head = "head",
+    Mask = "mask",
+    HairStyle = "hairstyle",
+    Torso = "torso",
+    Leg = "leg",
+    Bag = "bag",
+    Shoes = "shoes",
+    Accessories = "accessories",
+    Undershirts = "undershirts",
+    Armor = "armor",
+    Decal = "decal",
+    Top = "top",
+    Hat = "hat",
+    Glasses = "glasses",
+    Ears = "ears",
+    Null1 = "null1",
+    Null2 = "null2",
+    Null3 = "null3",
+    Watch = "watch",
+    Bracelets = "bracelets"
+}
