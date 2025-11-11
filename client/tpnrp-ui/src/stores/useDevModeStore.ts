e3fcf49f927430def97e5a15f64677f0cc8ab37a
Thien Tran <Thien.Tran@navigosgroup.com>
2025-11-11 15:40:45 +0700

    - Add UI Preview for quickly dev and test UI

--- client/tpnrp-ui/src/stores/useDevModeStore.ts
+++ client/tpnrp-ui/src/stores/useDevModeStore.ts
@@ -9,6 +9,7 @@ type DevModeState = {
   permission: string
   isDevModeOpen: boolean
   isConsoleOpen: boolean
+  isUIPreviewOpen: boolean
   consoleMessages: ConsoleMessage[]
   setDevModeOpen: (value: boolean) => void
   toggleDevMode: () => void
@@ -17,13 +18,16 @@ type DevModeState = {
   appendConsoleMessage: (message: ConsoleMessage) => void
   resetConsole: () => void
   setPermission: (value: string) => void
+  setUIPreviewOpen: (value: boolean) => void
 }
 
 export const useDevModeStore = create<DevModeState>((set) => ({
   permission: 'player',
   isDevModeOpen: false,
   isConsoleOpen: false,
+  isUIPreviewOpen: false,
   consoleMessages: [],
+  setUIPreviewOpen: (value: boolean) => set({ isUIPreviewOpen: value }),
   setDevModeOpen: (value) => set({ isDevModeOpen: value }),
   toggleDevMode: () =>
     set((state) => ({
